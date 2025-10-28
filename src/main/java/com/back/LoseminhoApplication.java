1ab06e5b84c2935cb7ae2e5420430db5a76cfd98
loseminho <loseminho@gmail.com>
2025-10-28 15:32:13 +0900

    start : 프로젝트 시작

---
+++ src/main/java/com/back/LoseminhoApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoseminhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoseminhoApplication.class, args);
+    }
+
+}
