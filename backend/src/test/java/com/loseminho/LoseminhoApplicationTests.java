570c53120a4e7f255205b25cf00795e066c7e3d6
loseminho <loseminho@gmail.com>
2025-10-30 15:27:50 +0900

    fix: .gitignore 수정 및 backend gradlew 추가

---
+++ backend/src/test/java/com/loseminho/LoseminhoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.loseminho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoseminhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 애플리케이션 컨텍스트 로드 테스트
+    }
+
+}
